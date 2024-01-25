88f5e2ba7dc532494a2f63d595020c15dafa9c93
jahanvi <jahanvibhatt1607@gmail.com>
2024-01-25 12:26:40 +0530

    function to merge objects added

--- Javascript/Practice 2/practice 2.5/script.js
+++ Javascript/Practice 2/practice 2.5/script.js
@@ -5,10 +5,23 @@ const obj2={
   name:"akash",
   age:24
 };
+function mergeObjects(obj1,obj2){
+  let result={};
+  for(let key in obj1){
+    result[key]=obj1[key];
+  }
+  for(let key in obj2){
+    result[key]=obj2[key];
+  }
+  return result;
+}
+let obj3=mergeObjects(obj1,obj2);
+console.log(obj3);
 
 // spread operator
-const obj3={...obj1,...obj2};
-console.log(obj3)
+// const obj3={...obj1,...obj2};
+// console.log(obj3)
 
+//assign method
 // const obj3 =Object.assign({},obj1, obj2);
 // console.log(obj3);
